47cd85ab78811acbbb4ce895e523f2a4993fb786
Rayberg <neformat30@gmail.com>
2025-06-18 19:09:06 +0300

    working without notifications

---
+++ MyStudy/myMusicApp/build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+    }
+}
+
+allprojects {
+    repositories {
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(buildDir)
+}
